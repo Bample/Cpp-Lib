574c588a7de8e200c7b5438309b59287cdeee589
awathefox <55276797+awathefox@users.noreply.github.com>
2020-06-12 20:53:21 +0800

    Type_traits 教学

--- Samples/templates/type_traits.cpp
+++ Samples/templates/type_traits.cpp
@@ -3,3 +3,31 @@
 struct t{};//true class
 struct f{};//false class
 template<class l>
+struct m{
+  typedef f p;
+}
+template<T>
+struct m<T*>{
+  typedef f p;
+}
+template<>
+struct m<int>{
+  typedef t p;
+}
+template<>
+struct m<char>{
+  typedef t p;
+}
+//...
+void r(f){std::cout<<"false"<<std::endl;}
+void r(t){std::cout<<"true"<<std::endl;}
+template<class n>
+void l(){
+  r(typename m<T>::p());
+}
+int main(){
+  l<int>();
+  l<string>();
+  l<f>();
+  return 0;
+}
